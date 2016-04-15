f5f521f842893fb4786a6755586964d717a3dd98
Travis CI <your@email.com>
2016-04-15 15:10:36 +0000

    Fix travis

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #execute this only when pull requesting to master, or pushing to master
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
 	set -e # exit with nonzero exit code if anything fails
 
 	# go to the directory which contains build artifacts and create a *new* Git repo
 	# directory may be different based on your particular build process
-	cd dist
+	cd disti
+	git init
 	# inside this git repo we'll pretend to be a new user
 
 	# The first and only commit to this new Git repo contains all the
@@ -15,16 +17,14 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 		mkdir $TRAVIS_PULL_REQUEST
 	        cp ./* $TRAVIS_PULL_REQUEST/ -Rv 2>/dev/null || :
 		rm -rf $TRAVIS_PULL_REQUEST/$TRAVIS_PULL_REQUEST/
-		git init
 		git add $TRAVIS_PULL_REQUEST/
 	else
-		git init
 		git add .
 	fi
 	git config user.name "Travis CI"
         git config user.email "<your@email.com>"
 	git commit -m "Deploy to GitHub Pages"
-
+git status
 	# Force push from the current repo's master branch to the remote
 	# repo's gh-pages branch. (All previous history on the gh-pages branch
 	# will be lost, since we are overwriting it.) We redirect any output to
