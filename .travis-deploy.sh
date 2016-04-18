7e6c3bcacd18c3b193632c587416da9784e33e53
Travis CI <your@email.com>
2016-04-18 13:49:17 +0000

    Add variables to travis.yml

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 #execute this only when pull requesting to master, or pushing to master
-export TRAVIS_PULL_REQUEST=9
 export TRAVIS_BRANCH=master
+export TRAVIS_PULL_REQUEST=8
+export GH_BRANCH=gh-pages
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-	set -e # exit with nonzero exit code if anything fails
+	set -e
+	rm -rf ../"${GH_BRANCH}"
+	git clone -b ${GH_BRANCH} "https://${GH_REF}" ../"${GH_BRANCH}"
 
-	# go to the directory which contains build artifacts and create a *new* Git repo
-	# directory may be different based on your particular build process
-	cd dist
-	git init
-	# inside this git repo we'll pretend to be a new user
-
-	# The first and only commit to this new Git repo contains all the
-	# files present with the commit message "Deploy to GitHub Pages".
 	if [ $TRAVIS_PULL_REQUEST ]; then
-		git fetch "https://${GH_REF}" gh-pages && git cherry-pick FETCH_HEAD
-		rm -rf $TRAVIS_PULL_REQUEST
-# run current changes
-		mkdir $TRAVIS_PULL_REQUEST
-	        mv ./* $TRAVIS_PULL_REQUEST/ -Rv 2>/dev/null || :
-		rm -rf $TRAVIS_PULL_REQUEST/$TRAVIS_PULL_REQUEST/
+		rm -rf ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST
+		mkdir ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST
+	       	cp ./dist/* ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST/ -rf 2>/dev/null || :
+	else
+		cp ./dist/* ../"${GH_BRANCH}" -rf 2>/dev/null || :
 	fi
+	cd ../"${GH_BRANCH}"
 	git add .
 	git config user.name "Travis CI"
         git config user.email "<your@email.com>"
@@ -31,5 +26,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 	# will be lost, since we are overwriting it.) We redirect any output to
 	# /dev/null to hide any sensitive credential data that might otherwise be exposed.
 	# tokens GH_TOKEN and GH_REF will be provided as Travis CI environment variables
-	git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 
+	git push --force "https://${GH_TOKEN}@${GH_REF}"
 fi
