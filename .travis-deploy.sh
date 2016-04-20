8e60b597c7be49e24475b17b5df215a4e5355ab2
Travis CI <your@email.com>
2016-04-20 10:02:19 +0000

    Add publish and fix deploy

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 #execute this only when pull requesting to master, or pushing to master
-export TRAVIS_BRANCH=master
-export TRAVIS_PULL_REQUEST=8
 export GH_BRANCH=gh-pages
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+# run this only if branch is master and we are not in pull request
+if [ "$TRAVIS_BRANCH" = "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]; then
 	set -e
 	rm -rf ../"${GH_BRANCH}"
 	git clone -b ${GH_BRANCH} "https://${GH_REF}" ../"${GH_BRANCH}"
 
-	if [ $TRAVIS_PULL_REQUEST ]; then
-		rm -rf ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST
-		mkdir ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST
-	       	cp ./dist/* ../"${GH_BRANCH}"/$TRAVIS_PULL_REQUEST/ -rf 2>/dev/null || :
-	else
-		cp ./dist/* ../"${GH_BRANCH}" -rf 2>/dev/null || :
-	fi
+	cp ./dist/* ../"${GH_BRANCH}" -rf 2>/dev/null || :
 	cd ../"${GH_BRANCH}"
+
 	git add .
 	git config user.name "Travis CI"
         git config user.email "<your@email.com>"
